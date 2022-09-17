a992726e3241b80b4c66077fb04128cd4524f1a1
Jaehyeok Seong <jaehyeokseong@Jaehyeoks-MacBook-Pro.local>
2022-09-18 01:27:17 +1000

    make source code

---
+++ source_code/SumAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SumAlgorithm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //[1] Input n명의 국어 점수
+            int[] scores = { 100, 75, 50, 37, 90, 95 };
+            int sum = 0;
+            //[2] Process: 합계 알고리즘 영역이다
+            foreach(int score in scores)
+            {
+                if(score >= 80)
+                {
+                    sum += score;
+                }
+            }
+            //[3] Output
+            Console.WriteLine("{0} 명의 점수 중 80점 이상의 총점: {1}", scores.Length, sum);
+        }
+    }
+}
